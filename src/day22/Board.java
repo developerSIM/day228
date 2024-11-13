8a2976779c47e917aa90a07c9182a9a45ab7648b
tlarjsqh <geonbosim318@gmail.com>
2024-11-13 19:39:28 +0900

    Changes

---
+++ src/day22/Board.java
@@ -0,0 +1,22 @@
+package day22;
+
+/*
+클래스 사용처
+1. main 함수를 갖는 실행 클래스, 프로젝트당 1개
+2. 객체를 생성하기 위한 설계 클래스
+
+
+ */
+
+public class Board { /* 게시물 타입 클래스 */
+/*
+        - 클래스 구성 멤버
+        1. 멤버 변수(필드)*/
+       String content; // --내용
+       String writer;
+       int pwd;
+
+
+
+
+}
