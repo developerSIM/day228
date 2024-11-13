47e2bdaa95d850e39565a464da6b24ac31adda29
tlarjsqh <geonbosim318@gmail.com>
2024-11-13 21:31:54 +0900

    Changes

--- src/day22/BoardService3.java
+++ src/day22/BoardService3.java
@@ -58,8 +58,8 @@ public class BoardService3 { /* 실행클래스 */
         // -입력객체
         Scanner scan = new Scanner(System.in);
         // - 고정길이 - Board 객체 100개를 저장할수 있는 배열 선언
-        Board[] boardList = new Board[100];
-
+        Board[] boardList = null; //변수는 그대로
+        int count = 0;
         while (true){
             System.out.print("1.글쓰기 2.글출력 : ");
             int choose = scan.nextInt();
@@ -71,18 +71,25 @@ public class BoardService3 { /* 실행클래스 */
                 // .nextLine() 사용시 주의할점 : .nextLine() 앞에 또 다른 .nextXX() 존재하면 의미없는 .nextLine() 작성해준다.
                 System.out.print("작성자 : ");     String writer = scan.next();
                 System.out.print("비밀번호 : ");    int pwd = scan.nextInt();
-                // [2] 배열내 빈 공간을 찾아서 게시물 작성하기 ,
-                for( int index = 0 ;  index <= boardList.length-1 ; index++ ){
-                    if( boardList[index] == null ){ // 만약에 index번째 게시물이 비어 있으면
-                        Board board = new Board();  // - 게시물 객체 생성
-                        // - 생성된 게시물 객체내 입력받은 값들을 대입한다.
-                        board.content = content; board.writer = writer; board.pwd = pwd;
-                        // - 배열내 index번째 위치에 생성한 객체를 저장/대입 한다.
-                        boardList[index] = board;
-                        break; // - 반복문을 종료한다.
+
+                // [2] 입력받은 데이터로 게시물 객체 생성
+                Board board = new Board();
+                board.content = content; board.writer = writer; board.pwd = pwd;
+
+                // [3]
+
+                count++;
+                Board[] newboardList = new Board[ count];
+                if ( count != 1){
+                    for( int index = 0 ; index <= boardList.length-1 ;  index++){
+                        newboardList[index] = boardList[index];
                     }
                 }
-            }else if( choose == 2 ){
+                newboardList[newboardList.length-1 ] = board;
+                boardList = newboardList;
+
+
+
 
                 // 배열내 존재하는 게시물 모두 출력하기
                 for( int index = 0 ; index <= boardList.length-1 ; index++ ){
